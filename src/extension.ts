3ad015346f144ad3497a888af60ded3d58bac82e
Tanmya Vishvakarma <tanmya2000@gmail.com>
2021-04-20 14:40:06 +0530

    fixed comm issue

--- src/extension.ts
+++ src/extension.ts
@@ -4,6 +4,9 @@ import Axios, { AxiosResponse } from "axios";
 import { Script } from 'node:vm';
 import { AsyncLocalStorage } from 'node:async_hooks';
 import {Memento} from 'vscode';
+import { insidersDownloadDirToExecutablePath } from 'vscode-test/out/util';
+import { constants } from 'node:buffer';
+import { parse } from 'node:path';
 export function activate(context: vscode.ExtensionContext) {
   vscode.window.registerTreeDataProvider('cricketCode',new TreeDataProvider);
  
@@ -36,7 +39,7 @@ export function activate(context: vscode.ExtensionContext) {
         );
     
     })
-    ,vscode.commands.registerCommand('cricketCode.getcommentary', async (id:string) => {
+    ,vscode.commands.registerCommand('cricketCode.getcommentary', async (matches:any[]) => {
     ;
       // Create and show a new webview
       const panel = vscode.window.createWebviewPanel(
@@ -47,12 +50,12 @@ export function activate(context: vscode.ExtensionContext) {
 			enableScripts: true}
       );
     const updateCommentaryWebview=async ()=>{
-	  panel.webview.html = await getCommentaryWebviewContent(id);
+	  panel.webview.html = await getCommentaryWebviewContent();
     };
 
     updateCommentaryWebview();
 
-    const interval=setInterval(updateCommentaryWebview,6000);
+    const interval=setInterval(updateCommentaryWebview,18000);
 
     panel.onDidDispose(
       () => {
@@ -71,7 +74,7 @@ async function getMatches() :Promise<any>
 
   const URL="https://cric-api.herokuapp.com/matches";
 
-  return   Axios.get(URL)
+  return Axios.get(URL)
   .then(
       (response: AxiosResponse<any>)=>{
         
@@ -105,20 +108,41 @@ function makeRow(team:any):string{
 
 function makeTable(matches:any[]) :string
 {
-  
+
+
   var table: string;
   table="<table>";
-  matches.map((match)=>{
+  var batar: any[]=[]
+  var idar: any[]=[]
+  var bowlar: any[]=[]
+  var comar:any[]=[]
+  var commentary;
+  var ctable:string;
+
+  matches.map((git:any[])=>{
+    batar.push(git.batting.team)
+    idar.push(git.id)
+    bowlar.push(git.bowling.team)
+  })
+  matches.map(async (match,index)=>{
     var batsmenRow=makeRow(match.batting);
+
     table+=batsmenRow;
 
     var bowlerRow=makeRow(match.bowling);
     table+=bowlerRow;
 
-    table+='<tr><td></td><tr>';
-    
+    // commentary=getcommentary(idar[index])
+    // ctable=makeComTable(await commentary);
+   
+    // comar.push(ctable);
+    // table+=ctable;
+    table+="<tr><td>"+match.status+"</td></tr>"
+    table+=`<tr><td></td><tr>`
+    // <button id="btn" onclick="showCom()"></button>
   });
   table+='</table>';
+
   return table;
 }
 
@@ -126,7 +150,7 @@ async function getMatchesWebviewContent() {
 
   var matches;
   var table:string;
-
+  var team_id:string;
   matches=getMatches();
 
   table=makeTable(await matches);
@@ -140,25 +164,70 @@ async function getMatchesWebviewContent() {
 	  <title>Cricket Code</title>
   </head>
   <body>
-  
+  <div id="table">
   `+table+`
+  </div>
+  <script>
+    function showCom(){
+      var btn=document.getElementById("btn");
+      btn.innerHTML="Click on Get Commentary";
+    }
+  </script>
   </body>
+  
   </html>`;
   
 	return HTML;
 }
 
+function getIndex(matches:any[]) {
+  var idar:any[]=[]
+  matches.map(gid=>{
+    idar.push(gid.id)
+  })
+  return idar
+}
 
-async function getCommentaryWebviewContent (id:string) {
-  id="35212"
+function getBat(matches:any[]) {
+  var teamar:any[]=[]
+  matches.map(gid=>{
+    teamar.push(gid.batting.team)
+  })
+  return teamar
+}
+function getBowl(matches:any[]) {
+  var teamar:any[]=[]
+  matches.map(gid=>{
+    teamar.push(gid.bowling.team)
+  })
+  return teamar
+}
+
+async function getCommentaryWebviewContent () {
+ 
+  var matches;
+  var idar: any[]=[]
+  // var allComs=[]
   var commentary;
-  var ctable:string;
-  commentary=getcommentary(id)
+  var bigdiv="<div>";
+  var batar;
+  var bowlar;
+  var ctable:string|undefined=undefined;
+  var buttons:string|""=""
+  matches=getMatches()
+  batar=getBat(await matches)
+  bowlar=getBowl(await matches)
+  idar=getIndex(await matches)
   
- 
-  ctable=makeComTable(await commentary);
-       
- 
+  
+   for(let i=0;i<idar.length;i++){
+    buttons+=`<button class=`+batar[i]+'/'+bowlar[i]+` id="btn${i}" onclick="whichCom(${i})">`+batar[i]+'/'+bowlar[i]+`</button>`
+    commentary=getcommentary(idar[i])
+    ctable=makeComTable(await commentary,i.toString()); 
+    ctable+='</table>'
+    bigdiv+=ctable
+  }
+  bigdiv+="</div>"
   var HTML=`<!DOCTYPE html>
   <html lang="en">
   <head>
@@ -167,13 +236,33 @@ async function getCommentaryWebviewContent (id:string) {
     <title>Cricket Code</title>
   </head>
   <body>
+  `+buttons+`
+  `+bigdiv+`
 
-  `+ctable+`
+  <script>
+
+  function whichCom(id){
+    var btn=document.getElementById('btn'+id);
+    
+    if(btn.innerText=="Hide Commentary"){
+      
+      document.getElementById(id).style.display="none";
+      btn.innerText=btn.className;
+    }else{
+ 
+    btn.innerText="Showing Commentary";
+    document.getElementById(id).style.display="block";
+    btn.innerText="Hide Commentary";
+    }
+  }
+  </script>
   </body>
   </html>`;
     
   return HTML;
     }
+
+
     async function getcommentary(id:string) :Promise<any>
     {
      
@@ -182,52 +271,48 @@ async function getCommentaryWebviewContent (id:string) {
       return   Axios.get(URL)
       .then(
           (response: AxiosResponse<any>)=>{
-           
             return  response.data;
-           
-          
           }
       )
       .catch((err)=>console.log(err));
     
   }
 
-    function makeComRow(comm:any):string{
+    // function makeComRow(comm:any):string{
 
-      var crow='<tr>'+'<td>'+comm.comm+': ';
-      comm.comm.map((sc: any,idx:number)=>{
-        if(idx!==0)
-        {
-          crow+=' & ';
-        }
-        if(sc.comm!==undefined)
-        {
-          var covers=sc.over!==undefined?'('+sc.overs+')':'';
-          crow+=covers;      
-        }
-      });
-      crow+='</td>'+'</tr>';
+    //   var crow='<tr>'+'<td>'+comm.comm+': ';
+    //   comm.comm.map((sc: any,idx:number)=>{
+    //     if(idx!==0)
+    //     {
+    //       crow+=' & ';
+    //     }
+    //     if(sc.comm!==undefined)
+    //     {
+    //       var covers=sc.over!==undefined?'('+sc.overs+')':'';
+    //       crow+=covers;      
+    //     }
+    //   });
+    //   crow+='</td>'+'</tr>';
     
-      return crow;
+    //   return crow;
     
-    }
+    // }
     
-    function makeComTable(commentary:any[]) :string
+    function makeComTable(commentary:any[],id:string) :string
     { 
       
      
       var ctable: string;
-      ctable="<table>";
-      console.log("kkkkkkkkkkkkkkk")
+      ctable="<table style=\"display:none;\"id="+id+">";
+     
       var newcom=[];
 
       newcom=commentary.comm;
-      console.log(newcom)
       newcom.map((com: any[])=>{
         var crow=`<tr><td>${com.comm}</td></tr>`
         ctable+=crow
       });
-    console.log(ctable)
+  
     return ctable
     }
 
@@ -263,3 +348,4 @@ async function getCommentaryWebviewContent (id:string) {
         this.children = children;
       }
     }
+    
